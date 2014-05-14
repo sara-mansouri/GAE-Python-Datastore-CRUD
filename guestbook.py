e2ecf13aa0b6ddc460a9ac6333e2542c4edcc753
smansoury <s.mansoury@gmail.com>
2014-05-14 13:15:20 -0600

    Edit and Delete

--- guestbook.py
+++ guestbook.py
@@ -4,20 +4,20 @@ import urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
+
 import jinja2
 import webapp2
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
 
 DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
 
-# We set a parent key on the 'Greetings' to ensure that they are all in the same
-# entity group. Queries across the single entity group will be consistent.
+# We set a parent key on the 'Greetings' to ensure that they are all in the
+# same
+# entity group.  Queries across the single entity group will be consistent.
 # However, the write rate should be limited to ~1/second.
-
 def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
     """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
     return ndb.Key('Guestbook', guestbook_name)
@@ -26,7 +26,8 @@ def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
 class Greeting(ndb.Model):
     """Models an individual Guestbook entry with author, content, and date."""
     author = ndb.UserProperty()
-    name = ndb.StringProperty(indexed=False)
+    
+    name = ndb.StringProperty(indexed=True)
     fname = ndb.StringProperty(indexed=False)
     email = ndb.StringProperty(indexed=False)
     content = ndb.StringProperty(indexed=False)
@@ -38,8 +39,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         guestbook_name = self.request.get('guestbook_name',
                                           DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
         greetings = greetings_query.fetch(10)
 
         if users.get_current_user():
@@ -60,12 +60,86 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
 
+    #def post(self):
+    #    delWhat = self.request.get("deletewhat")
+        
+    #    report = Greeting.query(Greeting.name == delWhat).get()
+    #    # Delete the entity
+    #    report.key.delete()
+    #    #ndb.Key(Greeting, int(delWhat)).delete()
+        
+    #    self.response.write(delWhat)
+
+    def post(self):
+        
+        edWhat = self.request.get("editwhat")
+        
+       # report = Greeting.query(Greeting.name==edWhat).get()
+        #report.key.delete()
+
+        #guestbook_name = self.request.get('guestbook_name',
+        #                                  DEFAULT_GUESTBOOK_NAME)
+        #report = Greeting(parent=guestbook_key(edWhat))
+        report = Greeting.query(Greeting.name == edWhat).get()
+        
+        template_values = {
+            'aaa_name': report.name,
+            'aaa_fname': report.fname,
+            'aaa_email': report.email,
+            'aaa_note': report.content,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+
+      
+        #report.name = self.request.get('name')
+        #report.fname = self.request.get('fname')
+        #report.email = self.request.get('email')
+        #report.content = self.request.get('content')
+        #report.put()
+        
+
+       # self.response.append('name':report.name)
+
+         #response.append({'user_id':
+       # self.response.write(delWhat)
+
+        
+class editing(webapp2.RequestHandler):
+
+    def post(self):
+        # We set the same parent key on the 'Greeting' to ensure each Greeting
+        # is in the same entity group.  Queries across the single entity group
+        # will be consistent.  However, the write rate to a single entity group
+        # should be limited to ~1/second.
+
+        edWhat = self.request.get("editwhat")
+
+        report = Greeting.query(Greeting.name == edWhat).get()
+
+        
+        #greeting.name = self.request.get('name')
+        #greeting.fname = self.request.get('fname')
+        #greeting.email = self.request.get('email')
+        #greeting.content = self.request.get('content')
+        #greeting.put()
+
+        report.name = self.request.get('name')
+        report.fname = self.request.get('fname')
+        report.email = self.request.get('email')
+        report.content = self.request.get('content')
+        report.put()
+        
+
+        
 class Guestbook(webapp2.RequestHandler):
 
     def post(self):
         # We set the same parent key on the 'Greeting' to ensure each Greeting
-        # is in the same entity group. Queries across the single entity group
-        # will be consistent. However, the write rate to a single entity group
+        # is in the same entity group.  Queries across the single entity group
+        # will be consistent.  However, the write rate to a single entity group
         # should be limited to ~1/second.
         guestbook_name = self.request.get('guestbook_name',
                                           DEFAULT_GUESTBOOK_NAME)
@@ -114,12 +188,11 @@ class MainHandler(webapp2.RequestHandler):
     def post(self):
         firstName = self.request.get("firstName")
         familyName = self.request.get("familyName")
-        self.response.out.write("First Name: " + firstName 
-           + " Family Name: " + familyName)
+        self.response.out.write("First Name: " + firstName + " Family Name: " + familyName)
  
 
-application = webapp2.WSGIApplication([
-    #('/', MainPage),
-    #('/sign', Guestbook),
-    ('/', MainHandler)
+application = webapp2.WSGIApplication([('/', MainPage),
+    ('/sign', Guestbook),
+    ('/edit', editing),
+    #('/', MainHandler)
 ], debug=True)
